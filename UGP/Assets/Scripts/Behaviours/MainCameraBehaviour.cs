3a27b00d4103f26406ec9ca05cd2069dae36b536
TrentButler <redtrentbutler@gmail.com>
2017-12-28 13:12:07 -0600

    vehicle movement

---
+++ UGP/Assets/Scripts/Behaviours/MainCameraBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Trent
+{
+    public class MainCameraBehaviour : MonoBehaviour
+    {
+        public Transform Target;
+        public Vector3 Offset;
+        public float CameraRotation;
+        public float cameraSpeed = 1.0f;
+        public float cameraTranslationSpeed = 1.0f;
+
+        public void ResetCamera()
+        {
+            transform.rotation = Quaternion.identity; //ZERO OUT THE ROTATION
+        }
+
+        private void FixedUpdate()
+        {
+            if(!Target)
+                #region MouseLook
+            //CALCULATE AN MOUSE DELTA
+            //DERIVE AN DIRECTION
+            //LERP BETWEEN THE CURRENT CAMERA ROTATION TO AN NEW CAMERA ROTATION
+            if (Input.GetMouseButton(1))
+            {
+                var deltaX = Input.GetAxis("Mouse X"); //GET THE MOUSE DELTA X
+                var deltaY = Input.GetAxis("Mouse Y"); //GET THE MOUSE DELTA Y
+
+                Vector3 rotX = new Vector3(-deltaY, 0, 0);
+                Vector3 rotY = new Vector3(0, deltaX, 0);
+
+                Quaternion rot = Quaternion.Euler(rotX); //CREATE A QUATERNION ROTATION FROM A EULER ANGLE ROTATION
+                transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * rot, 1.0f); //INTERPOLATE BETWEEN THE CURRENT ROTATION AND THE NEW ROTATION
+
+                rot = Quaternion.Euler(rotY); //CREATE A QUATERNION ROTATION FROM A EULER ANGLE ROTATION
+                transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * rot, 1.0f); //INTERPOLATE BETWEEN THE CURRENT ROTATION AND THE NEW ROTATION
+            }
+
+            //TRANSLATE THE CAMERA BASED ON CLICKDRAG DELTA
+            if (Input.GetMouseButton(2))
+            {
+                var dX = Input.GetAxis("Mouse X"); //GET THE DELTA MOUSE X
+                var dY = Input.GetAxis("Mouse Y"); //GET THE DELTA MOUSE Y
+
+                Vector3 trans = new Vector3(-(dX * cameraTranslationSpeed), -(dY * cameraTranslationSpeed), 0); //DERIVE A TRANSLATION VECTOR
+                GetComponent<Transform>().Translate(trans); //APPLY THE TRANSLATION TO THE CAMERA'S TRANSFORM
+            }
+
+            //MOVE THE CAMERA EITHER FORWARD OR BACKWARD FROM MOUSE SCROLL
+            var scrollDelta = Input.GetAxis("Mouse ScrollWheel"); //GET THE MOUSE SCROLL DELTA
+            Vector3 translation = new Vector3(0, 0, scrollDelta * cameraSpeed); //DERIVE A TRANSLATION VECTOR
+            GetComponent<Transform>().Translate(translation); //TRANSLATE THE CAMERA'S TRANSFORM
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //RESET THE CAMERA'S ROTATION TO ZERO
+                ResetCamera();
+            }
+            #endregion
+
+            if(Target)
+            {
+                //FOLLOW THE TARGET
+                var t = Target.position + Offset;
+                //GetComponent<Transform>().Translate(t);
+                GetComponent<Transform>().position = t;
+                GetComponent<Transform>().LookAt(Target);
+            }
+        }
+    }
+}
---
+++ UGP/Assets/Scripts/Behaviours/VehicleMovementBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Trent
+{
+    public class VehicleMovementBehaviour : MonoBehaviour
+    {
+        public float MaxSpeed = 1.0f;
+        public float TurnSpeed = 1.0f;
+        public float hoverStrength = 1.0f;
+        public float TargetHeight = 4.0f;
+
+        private Rigidbody rb;
+        
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+
+            if (!rb)
+                rb = gameObject.AddComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            var throttle = Input.GetAxis("Vertical");
+            var turnVehicle = Input.GetAxis("Horizontal");
+
+            Vector3 movementVector = new Vector3(turnVehicle * TurnSpeed, 0.0f, throttle * MaxSpeed);
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+            {
+                var vertForce = (TargetHeight - hit.distance) / TargetHeight;
+                Vector3 hoverVector = Vector3.up * vertForce * hoverStrength;
+                Debug.Log(hoverVector);
+
+                rb.AddForce(hoverVector);
+            }
+
+            rb.AddRelativeForce(movementVector);
+        }
+    }
+}
