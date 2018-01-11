c42ad9ea2a0c8da85a15a635a4d6fe9f2d4383a1
dylanmitchell1007 <armadummy@yahoo.com>
2018-01-11 17:59:07 -0600

    Computer-UI/Bench-UI

--- UGP/Assets/Scripts/Behaviours/GarageCameraBehaviour.cs
+++ UGP/Assets/Scripts/Behaviours/GarageCameraBehaviour.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 namespace Trent
 {
-    
+
     public class GarageCameraBehaviour : MonoBehaviour
     {
 
         public Transform Player;
-      
+
         public Transform SecurityCam1;
+
+        public Transform SecurityPost;
 
         public Transform ToolStationCam1;
 
@@ -23,19 +25,76 @@ namespace Trent
         public Collider ToolStationCollider;
 
         public Vector3 Offset;
-        
+
         // Use this for initialization
         void Start()
         {
             transform.position = SecurityCam1.position;
             transform.rotation = SecurityCam1.rotation;
             var player = FindObjectsOfType<PlayerMovementBehaviour>();
-            
+
+        }
+
+        public void PlayerSitCameraChange()
+        {
+            var player = FindObjectOfType<PlayerMovementBehaviour>();
+            if (player.IsSitting == true)
+            {
+                transform.position = ComputerCam1.position;
+                transform.rotation = ComputerCam1.rotation;
+
+            }
+
+            if (player.IsSitting == false)
+            {
+                transform.position = SecurityPost.position;
+                transform.rotation = SecurityPost.rotation;
+            }
+        }
+        public void PlayerStandCameraChange()
+        {
+            var player = FindObjectOfType<PlayerMovementBehaviour>();
+            if (player.IsStanding == true)
+            {
+                transform.position = ToolStationCam1.position;
+                transform.rotation = ToolStationCam1.rotation;
+            }
+            if (player.IsStanding == false)
+            {
+                transform.position = SecurityPost.position;
+                transform.rotation = SecurityPost.rotation;
+            }
+
+
         }
         void FixedUpdate()
         {
-            var player = FindObjectsOfType<PlayerMovementBehaviour>();
-            
+            PlayerStandCameraChange();
+            PlayerSitCameraChange();
+            //var player = FindObjectOfType<PlayerMovementBehaviour>();
+            //if(player.IsSitting == true)
+            //{
+            //    transform.position = ComputerCam1.position;
+            //    transform.rotation = ComputerCam1.rotation;
+
+            //}
+
+            //if(player.IsSitting == false)
+            //{
+            //    transform.position = SecurityPost.position;
+            //    transform.rotation = SecurityPost.rotation;
+            //}
+            //if(player.IsStanding == true)
+            //{
+            //    transform.position = ToolStationCam1.position;
+            //    transform.rotation = ToolStationCam1.rotation;
+            //}
+            //if(player.IsStanding == false)
+            //{
+            //    transform.position = SecurityPost.position;
+            //    transform.rotation = SecurityPost.rotation;
+            //}
+
             //if (player.transform.position == ComputerChair.transform.position &&  Input.GetKeyDown(KeyCode.E))
             //{
             //    player.position = PlayerSeat.position;
@@ -53,22 +112,23 @@ namespace Trent
             //    transform.position = ComputerCam1.position;
             //    transform.rotation = ComputerCam1.rotation;
             //}
-
-
         }
-        // Update is called once per frame
         void Update()
         {
 
         }
-
-
-
-        void OnTriggerEnter(Collider other)
-        {
-            if(other.tag == 
-        }
-
-
     }
 }
+
+
+
+// Update is called once per frame
+
+
+
+
+
+
+
+
+
--- UGP/Assets/Scripts/Behaviours/PlayerMovementBehaviour.cs
+++ UGP/Assets/Scripts/Behaviours/PlayerMovementBehaviour.cs
@@ -16,6 +16,10 @@ namespace Trent
         public float JumpStrength = 1.0f;
         public float TurnSpeed = 1.0f;
 
+        private bool CanMove;
+        public bool IsSitting = false;
+        public bool IsStanding = false;
+
         private bool HasJumped;
         private bool HasSprinted;
         private float SprintTimer = 1.0f;
@@ -24,8 +28,9 @@ namespace Trent
         // Use this for initialization
         void Start()
         {
+            CanMove = true;
             rb = GetComponent<Rigidbody>();
-            if(!rb)
+            if (!rb)
             {
                 rb = gameObject.AddComponent<Rigidbody>();
             }
@@ -34,78 +39,127 @@ namespace Trent
         {
             GetComponent<MeshRenderer>().material.color = Color.white;
         }
+        void OnTriggerEnter(Collider other)
+        {
+            var player = FindObjectsOfType<PlayerMovementBehaviour>();
+
+            if (other.tag == "ChairCollider")
+            {
+                transform.position = other.GetComponent<Transform>().position;
+                transform.rotation = other.GetComponent<Transform>().rotation;
+                Debug.Log("Test");
+                CanMove = false;
+                rb.isKinematic = true;
+                IsSitting = true;
+            }
+            if(other.tag == "ToolCollider")
+            {
+                transform.position = other.GetComponent<Transform>().position;
+                transform.rotation = other.GetComponent<Transform>().rotation;
+                Debug.Log("Test");
+                CanMove = false;
+                rb.isKinematic = true;
+                IsStanding = true;
+            }
+        }
+
         void FixedUpdate()
         {
-            if(!isLocalPlayer)
+            if(IsSitting)
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    //GET OUT OF CHAIR
+                    Vector3 getOutPosition = new Vector3(2.5f, 0, 0);
+                    transform.position = transform.position + getOutPosition;
+                    IsSitting = false;
+                    CanMove = true;
+                    rb.isKinematic = false;
+                }
+            if (IsStanding)
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    //GET OUT OF ToolBelt
+                    Vector3 getOutPosition = new Vector3(-1.5f, 0, 0);
+                    transform.position = transform.position + getOutPosition;
+                    IsStanding = false;
+                    CanMove = true;
+                    rb.isKinematic = false;
+                }
+
+            if (CanMove == true)
             {
-                return;
+                IsSitting = false;
+
+                if (!isLocalPlayer)
+                {
+                    return;
+                }
+                //OnStartLocalPlayer();
+                var Forward = Input.GetAxis("Vertical");
+                var TurnHead = Input.GetAxis("Horizontal");
+
+                Vector3 movementVector = new Vector3(TurnHead * TurnSpeed, 0.0f, Forward * WalkSpeed);
+
+                var deltaX = Input.GetAxis("Mouse X"); //GET THE MOUSE DELTA X
+                var deltaY = Input.GetAxis("Mouse Y"); //GET THE MOUSE DELTA Y
+
+                Vector3 rotX = new Vector3(-deltaY, 0, 0);
+                Vector3 rotY = new Vector3(0, deltaX, 0);
+
+                Quaternion rot = Quaternion.Euler(rotX); //CREATE A QUATERNION ROTATION FROM A EULER ANGLE ROTATION
+                transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * rot, 1.0f); //INTERPOLATE BETWEEN THE CURRENT ROTATION AND THE NEW ROTATION
+                rot = Quaternion.Euler(rotY); //CREATE A QUATERNION ROTATION FROM A EULER ANGLE ROTATION
+                transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * rot, 1.0f); //INTERPOLATE BETWEEN THE CURRENT ROTATION AND THE NEW ROTATION
+
+                if (Input.GetKeyDown(KeyCode.Space)) //JUMP
+                {
+                    Vector3 jumpVector = new Vector3(0, 1 * JumpStrength, 0);
+                    //rb.AddForce(jumpVector);
+                    HasJumped = true;
+                    transform.Translate(jumpVector);
+                    JumpTimer++;
+                }
+
+                if (JumpTimer >= 2.0f)
+                {
+                    JumpTimer = 0.0f;
+                    HasJumped = false;
+                }
+
+
+                if (HasJumped == true)
+                {
+                    JumpTimer += Time.deltaTime;
+                }
+
+
+
+                transform.Translate(movementVector);
+                //rb.AddForce(movementVector);
+
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    Vector3 sprintVector = new Vector3(Forward, WalkSpeed * RunSpeed);
+                    HasSprinted = true;
+                    //rb.AddForce(sprintVector);
+                    transform.Translate(sprintVector);
+                    SprintTimer++;
+                }
+
+                if (SprintTimer >= 2.0f)
+                {
+                    SprintTimer = 0.0f;
+                    HasSprinted = false;
+                }
+
+
+                if (HasSprinted == true)
+                {
+                    SprintTimer += Time.deltaTime;
+                }
+
+
             }
-            //OnStartLocalPlayer();
-            var Forward = Input.GetAxis("Vertical");
-            var TurnHead = Input.GetAxis("Horizontal");
-            
-            Vector3 movementVector = new Vector3(TurnHead * TurnSpeed, 0.0f, Forward * WalkSpeed);
-
-            var deltaX = Input.GetAxis("Mouse X"); //GET THE MOUSE DELTA X
-            var deltaY = Input.GetAxis("Mouse Y"); //GET THE MOUSE DELTA Y
-
-            Vector3 rotX = new Vector3(-deltaY, 0, 0);
-            Vector3 rotY = new Vector3(0, deltaX, 0);
-
-            Quaternion rot = Quaternion.Euler(rotX); //CREATE A QUATERNION ROTATION FROM A EULER ANGLE ROTATION
-            transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * rot, 1.0f); //INTERPOLATE BETWEEN THE CURRENT ROTATION AND THE NEW ROTATION
-           rot = Quaternion.Euler(rotY); //CREATE A QUATERNION ROTATION FROM A EULER ANGLE ROTATION
-            transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * rot, 1.0f); //INTERPOLATE BETWEEN THE CURRENT ROTATION AND THE NEW ROTATION
-        
-            if(Input.GetKeyDown(KeyCode.Space)) //JUMP
-            { 
-                Vector3 jumpVector = new Vector3(0, 1 * JumpStrength, 0);
-                //rb.AddForce(jumpVector);
-                HasJumped = true;
-                transform.Translate(jumpVector);
-                JumpTimer++;
-            }
-
-            if (JumpTimer >= 2.0f)
-            {
-                JumpTimer = 0.0f;
-                HasJumped = false;
-            }
-
-
-            if (HasJumped == true)
-            {
-                JumpTimer += Time.deltaTime;
-            }
-
-
-
-            transform.Translate(movementVector);
-            //rb.AddForce(movementVector);
-
-            if (Input.GetKeyDown(KeyCode.LeftShift)) 
-            {
-                Vector3 sprintVector = new Vector3(Forward, WalkSpeed * RunSpeed);
-                HasSprinted = true;
-                //rb.AddForce(sprintVector);
-                transform.Translate(sprintVector);
-                SprintTimer++;
-           }
-
-            if (SprintTimer >= 2.0f)
-            {
-                SprintTimer = 0.0f;
-                HasSprinted = false;
-            }
-
-
-            if (HasSprinted == true)
-            {
-                SprintTimer += Time.deltaTime;
-            }
-
-            
-            
         }
         // Update is called once per frame
         void Update()
