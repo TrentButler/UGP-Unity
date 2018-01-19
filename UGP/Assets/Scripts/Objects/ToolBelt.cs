ee31c7ed6535a03c16535f6223b06f69ade11a7b
TrentButler <redtrentbutler@gmail.com>
2018-01-19 00:13:23 -0600

    item systems

--- UGP/Assets/Scripts/Objects/ToolBelt.cs
+++ UGP/Assets/Scripts/Objects/ToolBelt.cs
@@ -1,58 +1,128 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace UGP
 {
     [CreateAssetMenu(fileName = "ToolBelt", menuName = "ToolBelt", order = 4)]
     public class ToolBelt : Item
     {
-        public List<Item> items = new List<Item>();
+        public List<Item> startItems = new List<Item>();
+
+        public Dictionary<string, List<Item>> toolBelt = new Dictionary<string, List<Item>>();
+
         public AmmoBox ammunition;
         public int capacity;
 
+        public void DumpStartItems()
+        {
+            startItems.ForEach(x => { AddItem(x); });
+        }
+
+        public void ClearToolBelt()
+        {
+            toolBelt.Clear();
+            toolBelt = new Dictionary<string, List<Item>>();
+        }
+        
         //NEEDS WORK
         public void AddItem(Item i)
         {
             //ITEMS ARE STACKABLE, BE SURE TO HANDLE THAT HERE
             //TEST OUT THE ADDING OF ITEMS, CHECK FOR 'type mismatch'
-
-            
-
             //DETERMINE THE TYPE OF THE ITEM THAT IS BEING ADDED,
             //USE A SWITCH STATEMENT TO HANDLE THE SPECIFIC ADDING OF THE ITEM TYPE
 
-            //var t = i.GetType();
+            string sItemType = i.GetType().ToString();
 
-            //switch(t)
-            //{
-            //    case Med:
-            //        {
-            //            break;
-            //        }
-            //}
-
-
-
-            if (items.Contains(i))
+            if(!toolBelt.ContainsKey(sItemType))
             {
-                var item = i;
-                Debug.Log(item);
+                //GENERATE A NEW KEY VALUE PAIR FOR THIS ITEM
+                toolBelt.Add(sItemType, new List<Item>() { i });
             }
 
-            if (!items.Contains(i))
-                if(items.Count < capacity)
-                    items.Add(i);
+            if(toolBelt.ContainsKey(sItemType))
+            {
+                //ADD THE ITEM TO ITS LIST
+                toolBelt[sItemType].Add(i);
+            }
         }
 
         public void RemoveItem(int index)
         {
-            if(items.Count < 0)
+            if(startItems.Count < 0)
             {
-                var removeThis = items[index];
+                var removeThis = startItems[index];
                 if (!removeThis)
-                    items.RemoveAt(index);
+                    startItems.RemoveAt(index);
             }   
         }
     }
+
+
+    #region TOOLBELT_INSPECTOR
+#if UNITY_EDITOR
+    [CustomEditor(typeof(ToolBelt))]
+    public class InspectorToolBelt : Editor
+    {
+        ToolBelt myTarget;
+
+        private void OnEnable()
+        {
+            myTarget = (ToolBelt)target;
+        }
+
+
+        //NEEDS WORK
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+
+            //ITERATE THROUGH THE DICTIONARY
+            //SAVE EACH ENTRY TO THE TOOLBELT 
+            //DISPLAY EACH ENTRY TO THE INSPECTOR
+            foreach(var k in myTarget.toolBelt.Keys)
+            {
+                EditorGUI.BeginChangeCheck();
+
+                List<Item> list = myTarget.toolBelt[k];
+
+                //var so = new SerializedObject(list.ToArray());
+                var so = new SerializedObject(myTarget);
+
+                var sp = so.FindProperty("targetObjects");
+
+                EditorGUILayout.PropertyField(sp, true);
+
+
+                so.ApplyModifiedProperties();
+                
+
+                EditorGUI.EndChangeCheck();
+
+                
+            }
+            
+            if(GUILayout.Button("Save ToolBelt"))
+            {
+                myTarget.DumpStartItems();
+            }
+
+            GUILayout.Space(10);
+            if(GUILayout.Button("Clear ToolBelt"))
+            {
+                myTarget.ClearToolBelt();
+            }
+        }
+
+
+    }
+
+#endif
+    #endregion
 }
