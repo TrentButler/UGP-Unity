e61645f53412280d38399b1c0065061334c25f04
dylanmitchell1007 <armadummy@yahoo.com>
2018-01-25 15:16:24 -0600

    Made NetworkHud behaviour script
    1/25/2018

---
+++ UGP/Assets/Scripts/Behaviours/NetworkMangerHUD.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkMangerHUD : NetworkBehaviour
+{
+    public Button StartHost;
+
+    public Button StartServer;
+
+    public Button StopHost;
+
+    public Button StartMatchmaker;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
