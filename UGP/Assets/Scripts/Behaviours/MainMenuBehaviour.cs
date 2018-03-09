995a70db3081a8f39e605bf1a9bd0f0c5be07867
dylanmitchell1007 <armadummy@yahoo.com>
2018-03-09 14:30:16 -0600

    Added LobbyManger

--- UGP/Assets/Scripts/Behaviours/MainMenuBehaviour.cs
+++ UGP/Assets/Scripts/Behaviours/MainMenuBehaviour.cs
@@ -11,16 +11,30 @@ public class MainMenuBehaviour : MonoBehaviour {
 
     public NetworkMangerHUD gudhud;
 
+    public GameObject MainMenuHUD;
+
+    public GameObject MultiplayerHUD;
     public void NetOn()
     {
         NetworkHud.SetActive(true);
+        MainMenuHUD.SetActive(false);
+    }
+    public void NetworkHUDOn()
+    {
+        MultiplayerHUD.SetActive(true);
+
+    }
+    public void NetworkHUDOFF()
+    {
+        MultiplayerHUD.SetActive(false);
     }
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         NetworkHud = GameObject.FindGameObjectWithTag("NetworkManager");
         NetworkHud.SetActive(false);
+        MultiplayerHUD.SetActive(false);
 	}
 	
 	// Update is called once per frame
