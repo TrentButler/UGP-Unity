4a2fcdbd6635e1aab867286d96c9cdba9d05490e
TrentButler <redtrentbutler@gmail.com>
2018-03-10 03:23:43 -0600

    add functionality for vehicle to 'slow down to a stop', begin moving the vehicle using it's rigidbody instead of 'transform.Translate'

--- UGP/Assets/Scripts/Behaviours/Offline/OfflineVehicleMovementBehaviour.cs
+++ UGP/Assets/Scripts/Behaviours/Offline/OfflineVehicleMovementBehaviour.cs
@@ -6,16 +6,24 @@ namespace UGP
 {
     public class OfflineVehicleMovementBehaviour : MonoBehaviour
     {
-        public float MaxSpeed = 1.0f;
-        private float originalSpeed;
-        public float TurnSpeed = 1.0f;
+        public Vector3 currenthovervector;
+
+        public Vector3 maxVehicleRotation;
+        public Vector3 minVehicleRotation;
+        public float vehicleRotateSpeed;
+
         public float hoverStrength = 1.0f;
         public float TargetHeight = 4.0f;
 
-        public float BoostSpeed = 1.0f;
         public float JumpStrength = 1.0f;
-        public float StrafeSpeed = 1.0f;
 
+        public float MaxSpeed = 1.0f;
+        private float originalSpeed;
+        public float StrafeSpeed = 1.0f;
+        private float originalStrafeSpeed;
+        public float RotateSpeed = 1.0f;
+        public float BoostSpeed = 1.0f;
+        
         public Transform ThrusterPosition;
         private Rigidbody rb;
 
@@ -56,6 +64,54 @@ namespace UGP
             transform.rotation = rot;
         }
 
+        private void CheckVehicleRotation()
+        {
+            //GET THE VEHICLE'S CURRENT ROTATION
+            //COMPARE EACH ROTATION AMOUNT TO THE MIN/MAX ROTATION
+            //IF ANY ROTATION IS PAST THE BOUNDARY, 
+            //LERP BETWEEN THE VALUES
+            var currentRot = transform.rotation;
+            var currentX = currentRot[0];
+            var currentY = currentRot[1];
+            var currentZ = currentRot[2];
+
+            //CHECK THE X ROTATION
+            if (currentX > maxVehicleRotation.x)
+            {
+                currentX = Mathf.LerpAngle(currentX, maxVehicleRotation.x, vehicleRotateSpeed * Time.smoothDeltaTime);
+            }
+            if (currentX < minVehicleRotation.x)
+            {
+                currentX = Mathf.LerpAngle(currentX, minVehicleRotation.x, vehicleRotateSpeed * Time.smoothDeltaTime);
+            }
+
+            //CHECK THE Y ROTATION
+            if (currentY > maxVehicleRotation.y)
+            {
+                currentY = Mathf.LerpAngle(currentY, maxVehicleRotation.y, vehicleRotateSpeed * Time.smoothDeltaTime);
+            }
+            if (currentY < minVehicleRotation.y)
+            {
+                currentY = Mathf.LerpAngle(currentY, minVehicleRotation.y, vehicleRotateSpeed * Time.smoothDeltaTime);
+            }
+
+            //CHECK THE Z ROTATION
+            if (currentZ > maxVehicleRotation.z)
+            {
+                currentZ = Mathf.LerpAngle(currentZ, maxVehicleRotation.z, vehicleRotateSpeed * Time.smoothDeltaTime);
+            }
+            if (currentZ < minVehicleRotation.z)
+            {
+                currentZ = Mathf.LerpAngle(currentZ, minVehicleRotation.z, vehicleRotateSpeed * Time.smoothDeltaTime);
+            }
+
+            currentRot[0] = currentX;
+            currentRot[1] = currentY;
+            currentRot[2] = currentZ;
+
+            transform.rotation = currentRot;
+        }
+
         public void Steer()
         {
             var dX = Input.GetAxis("Mouse X");
@@ -64,7 +120,7 @@ namespace UGP
 
             if (mouseYRot.magnitude > 0)
             {
-                transform.Rotate(mouseYRot, Space.Self);
+                transform.Rotate(mouseYRot * RotateSpeed, Space.Self);
             }
             rb.MoveRotation(transform.rotation);
         }
@@ -94,28 +150,47 @@ namespace UGP
             }
         }
 
+        public void ApplyBreak()
+        {
+            if (Input.GetKey(KeyCode.C)) //SLOW DOWN TO A STOP
+            {
+                MaxSpeed = 0.0f;
+                StrafeSpeed = 0.0f;
+                rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, Time.fixedDeltaTime);
+            }
+            else
+            {
+                MaxSpeed = originalSpeed;
+                StrafeSpeed = originalStrafeSpeed;
+            }
+        }
+
+        //NEEDS WORK
+        //WHEN APPLYING A FORCE BACKWARDS (S KEY PRESS), VEHICLE MOVES UPWARD/BACKWARD TOWARD THE CAMERA
         public void Move()
         {
             var throttle = Input.GetAxis("Vertical");
             var strafeVehicle = Input.GetAxis("Horizontal");
 
             Vector3 accelerationVector = new Vector3(0.0f, 0.0f, throttle * MaxSpeed);
-            Vector3 strafeVector = new Vector3(strafeVehicle * TurnSpeed, 0, 0.0f);
+            Vector3 strafeVector = new Vector3(strafeVehicle * StrafeSpeed, 0, 0.0f);
 
             #region HOVERVECTORCALCULATION
             //PERFORM A RAYCAST DOWNWARD, 
             //CALCULATE THE DISTANCE FROM BOTTOM OF VEHICLE TO THE GROUND
             //GENERATE A 'hoverVector' BASED ON THIS CALCULATION
-
             RaycastHit hit;
+            //var world_point = transform.TransformPoint(point.position);
             if (Physics.Raycast(rb.worldCenterOfMass, -Vector3.up, out hit))
             {
                 var vertForce = (TargetHeight - hit.distance) / TargetHeight;
                 Vector3 hoverVector = Vector3.up * vertForce * hoverStrength;
 
                 //Debug.Log(hoverVector); //DELETE THIS
+                currenthovervector = hoverVector;
 
                 rb.AddForce(hoverVector);
+                //rb.AddForceAtPosition(hoverVector, point.position);
             }
             #endregion
 
@@ -124,24 +199,13 @@ namespace UGP
                 UseBooster();
                 Jump();
 
-                //MOVE FORWARD BASED ON THE TRANSFORM'S FORWARD
-                var currentVelocity = rb.velocity;
-                if (currentVelocity.magnitude < MaxSpeed)
-                {
-                    var accel = (accelerationVector + ThrusterPosition.forward);
-                    var strafe = (new Vector3(strafeVehicle, 0, 0) * StrafeSpeed);
+                var cam_transform = Camera.main.transform;
+                var move_direction = cam_transform.TransformDirection(((accelerationVector) + strafeVector));
 
-                    transform.Translate((accel + strafe) * Time.fixedDeltaTime);
-                    rb.MovePosition(transform.position);
-                    //rb.AddForce(0, 0, throttle, ForceMode.Impulse);
-                    //rb.AddForceAtPosition()
-                }
+                rb.AddForce(move_direction, ForceMode.Impulse);
             }
-            //else
-            //{
-            //    rb.velocity = Vector3.zero;
-            //    rb.rotation = transform.rotation;
-            //}
+            
+            ApplyBreak();
             Steer();
         }
         #endregion
@@ -154,6 +218,7 @@ namespace UGP
                 rb = gameObject.AddComponent<Rigidbody>();
 
             originalSpeed = MaxSpeed;
+            originalStrafeSpeed = StrafeSpeed;
         }
 
         void FixedUpdate()
@@ -161,9 +226,10 @@ namespace UGP
             Move();
         }
 
-        private void LateUpdate()
+        private void Update()
         {
-            KeepVehicleUpright();
+            //KeepVehicleUpright();
+            CheckVehicleRotation();
         }
     }
 }
