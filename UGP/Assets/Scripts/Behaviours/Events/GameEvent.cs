8e0fd9d436f4bbedf161d1cccc4584cc474dd0c5
dylanmitchell1007 <armadummy@yahoo.com>
2018-03-15 10:52:51 -0500

    Add Event system

---
+++ UGP/Assets/Scripts/Behaviours/Events/GameEvent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace UGPEVENTS
+{
+
+    [CreateAssetMenu]
+    public class GameEvent : ScriptableObject
+    {
+        private List<GameEventListener> listeners = new List<GameEventListener>();
+
+
+
+        public void Raise()
+        {
+            for (int i = listeners.Count - 1; i >= 0; i--)
+            {
+                listeners[i].OnEventRaised();
+            }
+        }
+        public void RegisterListener(GameEventListener listener)
+        {
+            if (listeners.Contains(listener))
+            {
+                throw new InvalidOperationException("Duplicate key");
+            }
+            listeners.Add(listener);
+        }
+        /// <summary>
+        /// remove a listener, this will happen when the listener object becomes disabled
+        /// </summary>
+        /// <param name="listener">the monobehaviour we are listening 'from'</param>
+        public void UnregisterListener(GameEventListener listener)
+        {
+            if (listeners.Contains(listener))
+            {
+                listeners.Remove(listener);
+            }
+            else
+            {
+                throw new InvalidOperationException("No listener to remove");
+            }
+        }
+
+    }
+}
---
+++ UGP/Assets/Scripts/Behaviours/Events/GameEventArgsResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UGPEVENTS
+{
+
+    public class GameEventArgsResponse : UnityEngine.Events.UnityEvent<UnityEngine.Object[]>
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
