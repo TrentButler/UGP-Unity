8aa5599bd1849a332ae8705f8cb2325dfc9a4681
TrentButler <redtrentbutler@gmail.com>
2018-03-14 23:07:23 -0500

    create 'inputcontroller ' class, create 'defaultvehiclecontroller' class, issues with assigning the derived class 'defaultvehiclecontroller' to the 'inputcontroller' refrence on the script 'networkusercontrol'

---
+++ UGP/Assets/Scripts/Behaviours/Input/InputController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UGP
+{
+    public abstract class InputController : MonoBehaviour
+    {
+        public abstract void Move(float x, float y);
+    }
+
+}
---
+++ UGP/Assets/Scripts/Behaviours/Input/NetworkUserControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace UGP
+{
+    public class NetworkUserControl : NetworkBehaviour
+    {
+        public string InputHorizontal = "Horizontal";
+        public string InputVertical = "Vertical";
+
+        public DefaultVehicleController ic;
+
+        void FixedUpdate()
+        {
+            if(!isLocalPlayer)
+            {
+                if(hasAuthority)
+                {
+                    var _h = Input.GetAxis(InputHorizontal);
+                    var _v = Input.GetAxis(InputVertical);
+                    ic.Move(_h, _v);
+                    return;
+                }
+
+                return;
+            }
+
+            var h = Input.GetAxis(InputHorizontal);
+            var v = Input.GetAxis(InputVertical);
+            ic.Move(h, v);
+        }
+    }
+}
