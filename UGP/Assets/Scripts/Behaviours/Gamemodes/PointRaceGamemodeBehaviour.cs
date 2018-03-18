8eb94b45c28a25617d3c274be5a3253a716143a5
TrentButler <redtrentbutler@gmail.com>
2018-03-18 02:47:22 -0500

    needs testing

---
+++ UGP/Assets/Scripts/Behaviours/Gamemodes/PointRaceGamemodeBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace UGP
+{
+    public class PointRaceGamemodeBehaviour : NetworkBehaviour
+    {
+        public List<PlayerBehaviour> players;
+        public Transform Finish;
+
+        public InGameNetworkBehaviour net;
+        public Text t;
+
+        private void Start()
+        {
+            if(isServer)
+            {
+                players = FindObjectsOfType<PlayerBehaviour>().ToList();
+                net.SpawnAllVehicles();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if(isServer)
+            {
+                //SORT THE LIST OF PLAYERS BY THEIR DISTANCE TO THE 'FINISH'
+                var sorted_players = players.OrderBy(x => Vector3.Distance(x.transform.position, Finish.transform.position)).ToList();
+                players = sorted_players;
+
+                var first = "FIRST PLACE: " + players[0].name + "\n";
+                var second = "SECOND PLACE: " + players[1].name + "\n";
+                var third = "THRID PLACE: " + players[2].name + "\n";
+
+                t.text = first + second + third;
+            }
+        }
+    }
+}
---
+++ UGP/Assets/Scripts/Behaviours/GetPlayerLobbyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetPlayerLobbyInfo : Prototype.NetworkLobby.LobbyHook
+{
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        var lb = lobbyPlayer.GetComponent<Prototype.NetworkLobby.LobbyPlayer>();
+        gamePlayer.name = lb.playerName;
+
+        base.OnLobbyServerSceneLoadedForPlayer(manager, lobbyPlayer, gamePlayer);
+    }
+}
