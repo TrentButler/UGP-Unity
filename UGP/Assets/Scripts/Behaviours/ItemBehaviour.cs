d5414e27cbae1b99ec0e2fd9df6797a8455e8747
dylanmitchell1007 <armadummy@yahoo.com>
2018-03-21 17:50:52 -0500

    Test Item pickup

---
+++ UGP/Assets/Scripts/Behaviours/ItemBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UGP
+{
+
+    public class ItemBehaviour : MonoBehaviour
+    {
+        public GameObject Item;
+
+
+       
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
